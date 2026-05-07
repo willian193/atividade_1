c8ca4ff8ffa10377ca3ea0282b0e51cb33a94df9
willian193 <willianradunzz24@gmail.com>
2026-05-07 12:32:01 -0300

    feito

--- src/main/java/com/example/atividade_1/model/Aluno.java
+++ src/main/java/com/example/atividade_1/model/Aluno.java
@@ -1,5 +1,19 @@
 package com.example.atividade_1.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Aluno {
 
     private String nome;
