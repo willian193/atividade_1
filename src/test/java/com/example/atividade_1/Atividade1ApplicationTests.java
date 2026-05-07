e76105c037dda432ad90edc0b16acc69f07db574
willian193 <willianradunzz24@gmail.com>
2026-05-07 11:34:46 -0300

    Initial commit

---
+++ src/test/java/com/example/atividade_1/Atividade1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividade_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
